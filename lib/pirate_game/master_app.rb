0b6787f852e4a3978da773537421029a18596951
Davy Stevenson <davy.stevenson@gmail.com>
2013-10-12 17:47:13 -0700

    Updating the styling of master_app

--- lib/pirate_game/master_app.rb
+++ lib/pirate_game/master_app.rb
@@ -10,17 +10,23 @@ class PirateGame::MasterApp
 
   def run
     Shoes.app width: 360, height: 360, resizeable: false, title: 'Game Master' do
+
+      @dark_color = dimgray
+      @light_color = gainsboro
+
       def launch_screen
         clear do
-          background black
-          title "Start Game", stroke: white
-          edit_line text: 'Name' do |s|
-            @name = s.text
+          background @dark_color
+          stack margin: 20 do
+            title "Start Game", stroke: @light_color
+            edit_line text: 'Name' do |s|
+              @name = s.text
+            end
+            button('launch') {
+              @game_master = PirateGame::GameMaster.new(name: @name)
+              display_screen
+            }
           end
-          button('launch') {
-            @game_master = PirateGame::GameMaster.new(name: @name)
-            display_screen
-          }
         end
       end
 
@@ -28,24 +34,26 @@ class PirateGame::MasterApp
         @game_master.update
 
         clear do
-          background "#ffffff"
+          background @light_color
 
           stack :margin => 20 do
-            title "Game #{@game_master.name}"
+            title "Game #{@game_master.name}", stroke: @dark_color
 
-            @stuff = stack
+            @button_stack = stack
+            @registrations = para stroke: @dark_color
+            @stage_info = para stroke: @dark_color
           end
           animate(5) {
-            @stuff.clear do
+            @button_stack.clear do
               if @game_master.startable?
-                button ('start stage') {
+                button('start stage') {
                   @game_master.start
                 }
               end
-
-              para @game_master.registrations_text
-              para @game_master.stage_info
             end
+
+            @registrations.replace @game_master.registrations_text
+            @stage_info.replace @game_master.stage_info
           }
         end
       end
