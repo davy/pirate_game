f960680388f876c2724e2ad038237fe17af32b0f
Davy Stevenson <davy.stevenson@gmail.com>
2013-10-13 13:20:31 -0700

    Namespace app files and use Boot

--- lib/pirate_game/client_app.rb
+++ lib/pirate_game/client_app.rb
@@ -1,151 +1,150 @@
+module PirateGame
+  class ClientApp
 
-class PirateGame::ClientApp
+    def self.run
+      @my_app = Shoes.app width: 360, height: 360, resizeable: false, title: 'Pirate Game' do
 
-  def self.run
-    @my_app = Shoes.app width: 360, height: 360, resizeable: false, title: 'Pirate Game' do
+        @client = nil
 
-      @dark_color = dimgray
-      @light_color = gainsboro
-      @sky_color = aqua
-      @pub_color = darkred
-
-      @client = nil
-
-      def launch_screen
-        clear do
-          background @dark_color
-          stack margin: 20 do
-            title "What's your name", stroke: @light_color
-            el = edit_line text: 'Name' do |s|
-              @name = s.text
-            end
-            button('launch') {
-              @client = PirateGame::Client.new(name: @name)
-              select_game_screen
-            }
-          end
-        end
-      end
-
-      def select_game_screen
-        clear do
-          background @dark_color
-          stack :margin => 20 do
-            title "Choose Game", stroke: @light_color
-
-            stack do
-              motherships = @client.find_all_motherships
-
-              if motherships.empty?
-                subtitle "No Games Found", stroke: @light_color
-              else
-                subtitle "Select Game", stroke: @light_color
+        def launch_screen
+          clear do
+            background Boot::COLORS[:dark]
+            stack margin: 20 do
+              title "What's your name", stroke: Boot::COLORS[:light]
+              el = edit_line text: 'Name' do |s|
+                @name = s.text
               end
-              for mothership in motherships
-                button(mothership[:name]) {|b|
-                  begin
-                    @client.initiate_communication_with_mothership(b.text)
-                    @client.register
-                  rescue
-                    select_game_screen
-                  end
-                  pub_screen
-                }
-              end
-
-              button('rescan') {
+              button('launch') {
+                @client = Client.new(name: @name)
                 select_game_screen
               }
             end
           end
         end
-      end
 
-      def pub_screen
-        clear do
-          background @pub_color
-          stack :margin => 20 do
-            title "Pirate Pub", stroke: @light_color
-            tagline "Welcome #{@client.name}", stroke: @light_color
+        def select_game_screen
+          clear do
+            background Boot::COLORS[:dark]
+            stack :margin => 20 do
+              title "Choose Game", stroke: Boot::COLORS[:light]
 
-            stack do @status = para end
+              stack do
+                motherships = @client.find_all_motherships
 
-            @registered = nil
-            @updating_area = stack
-            @chat_room = stack
+                if motherships.empty?
+                  subtitle "No Games Found", stroke: Boot::COLORS[:light]
+                else
+                  subtitle "Select Game", stroke: Boot::COLORS[:light]
+                end
+                for mothership in motherships
+                  button(mothership[:name]) {|b|
+                    begin
+                      @client.initiate_communication_with_mothership(b.text)
+                      @client.register
+                    rescue
+                      select_game_screen
+                    end
+                    pub_screen
+                  }
+                end
+
+                button('rescan') {
+                  select_game_screen
+                }
+              end
+            end
           end
+        end
 
-          # checks for registration changes
-          # updates chat messages
-          animate(5) {
-            if @client
+        def pub_screen
+          clear do
+            background Boot::COLORS[:pub]
+            stack :margin => 20 do
+              title "Pirate Pub", stroke: Boot::COLORS[:light]
+              tagline "Welcome #{@client.name}", stroke: Boot::COLORS[:light]
 
-              detect_registration_change
+              stack do @status = para end
 
-              if @registered
-                @chat_room.clear do
-                  caption "In the Pub", stroke: @light_color
-                  para @client.teammates.join(', '), stroke: @light_color
-                  for msg, name in @client.msg_log
-                    para "#{name} said: #{msg}", stroke: @light_color
+              @registered = nil
+              @updating_area = stack
+              @chat_room = stack
+            end
+
+            # checks for registration changes
+            # updates chat messages
+            animate(5) {
+              if @client
+
+                detect_registration_change
+
+                if @registered
+                  @chat_room.clear do
+                    caption "In the Pub", stroke: Boot::COLORS[:light]
+                    para @client.teammates.join(', '), stroke: Boot::COLORS[:light]
+                    for msg, name in @client.msg_log
+                      para "#{name} said: #{msg}", stroke: Boot::COLORS[:light]
+                    end
                   end
                 end
               end
-            end
-          }
-          animate(5) {
-            stage_screen if @client.bridge
-          }
+            }
+            animate(5) {
+              stage_screen if @client.bridge
+            }
+          end
         end
-      end
 
-      def stage_screen
-        clear do
-          background @sky_color
-          stack :margin => 20 do
-            title "Ahoy!", stroke: @dark_color
+        def stage_screen
+          clear do
+            background Boot::COLORS[:sky]
+            stack :margin => 20 do
+              title "Ahoy!", stroke: Boot::COLORS[:dark]
 
-            @button_flow = flow do
-              for item in @client.bridge.items
-                button(item) {|b| @client.perform_action b.text }
+              @button_flow = flow do
+                for item in @client.bridge.items
+                  button(item) {|b| @client.perform_action b.text }
+                end
+              end
+            end
+            animate(5) {
+              pub_screen unless @client.bridge
+            }
+          end
+        end
+
+        def detect_registration_change
+          if @registered != @client.registered?
+            @registered = @client.registered?
+            @status.replace "#{"Not " unless @registered}Registered"
+            @updating_area.clear do
+              if @registered
+                button("Test Action") { @client.perform_action 'Test Action' }
+
+                el = edit_line
+
+                button("Send") {
+                  @client.broadcast(el.text)
+                  el.text = ''
+                }
+              else
+                button("Register")    { register }
               end
             end
           end
         end
-      end
 
-      def detect_registration_change
-        if @registered != @client.registered?
-          @registered = @client.registered?
-          @status.replace "#{"Not " unless @registered}Registered"
-          @updating_area.clear do
-            if @registered
-              button("Test Action") { @client.perform_action 'Test Action' }
-
-              el = edit_line
-
-              button("Send") {
-                @client.broadcast(el.text)
-                el.text = ''
-              }
-            else
-              button("Register")    { register }
-            end
-          end
+        def register
+          @client.register if @client
         end
-      end
 
-      def register
-        @client.register if @client
-      end
+        def unregister
+          @client.unregister if @client
+        end
 
-      def unregister
-        @client.unregister if @client
+        launch_screen
       end
-
-      launch_screen
+    ensure
+      @my_app.unregister if @my_app
     end
-  ensure
-    @my_app.unregister if @my_app
   end
 end
--- lib/pirate_game/master_app.rb
+++ lib/pirate_game/master_app.rb
@@ -1,65 +1,78 @@
-class PirateGame::MasterApp
+module PirateGame
+  class MasterApp
 
-  def self.run
-    new.run
-  end
+    def self.run
+      new.run
+    end
 
-  def initialize
-    @game_master = nil
-  end
+    def initialize
+      @game_master = nil
+    end
 
-  def run
-    Shoes.app width: 360, height: 360, resizeable: false, title: 'Game Master' do
+    def run
+      Shoes.app width: 360, height: 360, resizeable: false, title: 'Game Master' do
 
-      @dark_color = dimgray
-      @light_color = gainsboro
 
-      def launch_screen
-        clear do
-          background @dark_color
-          stack margin: 20 do
-            title "Start Game", stroke: @light_color
-            edit_line text: 'Name' do |s|
-              @name = s.text
+        def launch_screen
+          clear do
+            background Boot::COLORS[:dark]
+            stack margin: 20 do
+              title "Start Game", stroke: Boot::COLORS[:light]
+              edit_line text: 'Name' do |s|
+                @name = s.text
+              end
+              button('launch') {
+                @game_master = GameMaster.new(name: @name)
+                display_screen
+              }
             end
-            button('launch') {
-              @game_master = PirateGame::GameMaster.new(name: @name)
-              display_screen
+          end
+        end
+
+        def display_screen
+          @game_master.update
+
+          clear do
+            background Boot::COLORS[:light]
+
+            stack :margin => 20 do
+              title "Game #{@game_master.name}", stroke: Boot::COLORS[:dark]
+
+              @button_stack = stack
+              @registrations = para stroke: Boot::COLORS[:dark]
+              @stage_info = para stroke: Boot::COLORS[:dark]
+            end
+            animate(5) {
+              @button_stack.clear do
+                if @game_master.startable?
+                  button('start stage') {
+                    @game_master.start
+                  }
+                end
+              end
+
+              @registrations.replace @game_master.registrations_text
+              @stage_info.replace @game_master.stage_info
             }
           end
         end
-      end
 
-      def display_screen
-        @game_master.update
+        def detect_status_change
+          if @startable != @game_master.startable?
+            @startable = @game_master.startable?
 
-        clear do
-          background @light_color
-
-          stack :margin => 20 do
-            title "Game #{@game_master.name}", stroke: @dark_color
-
-            @button_stack = stack
-            @registrations = para stroke: @dark_color
-            @stage_info = para stroke: @dark_color
-          end
-          animate(5) {
-            @button_stack.clear do
-              if @game_master.startable?
+            @stuff.clear do
+              if @startable
                 button('start stage') {
                   @game_master.start
                 }
               end
             end
-
-            @registrations.replace @game_master.registrations_text
-            @stage_info.replace @game_master.stage_info
-          }
+          end
         end
-      end
 
-      launch_screen
+        launch_screen
+      end
     end
   end
 end
-
