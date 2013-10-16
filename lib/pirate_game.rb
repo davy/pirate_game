2f6a168d2dd98864a763335a069a6e85534fd654
Eric Hodel <drbrain@segment7.net>
2013-10-15 22:37:09 -0700

    Sort require list

--- lib/pirate_game.rb
+++ lib/pirate_game.rb
@@ -2,13 +2,13 @@ module PirateGame
   VERSION = '0.0.1'
 end
 
-require 'pirate_game/client_app'
+require 'pirate_game/boot'
+require 'pirate_game/bridge'
 require 'pirate_game/client'
+require 'pirate_game/client_app'
 require 'pirate_game/game_master'
 require 'pirate_game/master_app'
-require 'pirate_game/stage'
-require 'pirate_game/bridge'
 require 'pirate_game/protocol'
+require 'pirate_game/stage'
 require 'pirate_game/timeout_renewer'
-require 'pirate_game/boot'
 
