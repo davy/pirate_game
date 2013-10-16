c30f22ecd688db5bf17213651d9c94f44a25b275
Eric Hodel <drbrain@segment7.net>
2013-10-15 21:07:23 -0700

    Rewrite launch screen using objects
    I would like to integrate the launch screen into the app better, but it
    is difficult to do.
    
    I rewrote the launch screen to separate drawing from animation which
    works better with shoes.  I created a Wave and Ship object which have
    separate draw and animate methods.  The draw method puts things on the
    screen and the animate moves it around based on the current frame.
    
    This makes the launch_screen method much easier to understand as each
    part (setup, draw, animate) is separate from the others.

--- lib/pirate_game_launch_screen.rb
+++ lib/pirate_game_launch_screen.rb
@@ -1,54 +1,120 @@
 require 'pirate_game'
 
 module PirateGame
-  Shoes.app width: 360, height: 360, resizeable: false, title: 'Pirate Game' do
 
-    def draw_wave(top, frame, seed)
-      size = 40
+  class Image
 
-      offset_x, offset_y = Boot.generate_waving_x_y_offsets(frame, seed, size, 5)
+    def initialize shoes, image, top, left
+      @shoes = shoes
+      @image = image
+      @top   = top
+      @left  = left
+    end
 
-      nofill
-      colors = Boot::BLUE_COLORS + Boot::GREEN_COLORS
+    def animate frame
+      top_offset, left_offset =
+        Boot.generate_waving_x_y_offsets(frame, 0, 10, 4)
 
+      @ship.move @top + top_offset, @left + left_offset
+    end
+
+    def draw
+      @ship = @shoes.image @image, top: @top, left: @left
+    end
+
+  end
+
+  class Wave
+
+    COLORS = Boot::BLUE_COLORS + Boot::GREEN_COLORS
+
+    OFFSET = 230
+
+    SIZE   = 40
+
+    def initialize shoes, top, seed
+      @shoes = shoes
+      @top   = top + OFFSET
+      @seed  = seed
+
+      @arcs  = []
+      @clear = @shoes.rgb 0, 0, 0, 0
+    end
+
+    def animate frame
+      top_offset, left_offset =
+        Boot.generate_waving_x_y_offsets(frame, @seed, SIZE, 5)
+
+      @arcs.each do |arc, top, left|
+        arc.move top + top_offset, left + left_offset
+      end
+    end
+
+    def draw
       for i in (-1..10) do
         for j in [0,2,4] do
-
-          color_index = (seed + j) % colors.size
-          stroke colors[color_index]
-          strokewidth 3
+          color_index = (@seed + j) % COLORS.size
+          @shoes.fill @clear
+          @shoes.stroke COLORS[color_index]
+          @shoes.strokewidth 3
           arc_dif = (j/360.0)*Shoes::TWO_PI
-          arc(i*size + offset_x - j, top + j + offset_y, size + j, size + j, 0 + arc_dif, Shoes::PI - arc_dif)
+
+          top  = i * SIZE - j
+          left = @top + j
+
+          arc = @shoes.arc(i * SIZE - j, @top + j, SIZE + j, SIZE + j,
+                           0 + arc_dif, Shoes::PI - arc_dif)
+
+          @arcs << [arc, top, left]
         end
       end
     end
 
-    def draw_ship(frame, top, left, file=nil)
-      offset_x, offset_y = Boot.generate_waving_x_y_offsets(frame, 0, 10, 4)
+  end
 
-      file ||= '/../imgs/pirate_ship_sm.png'
-      ship_file = File.dirname(__FILE__) + file
-      ship = image(ship_file, top: top + offset_y, left: left + offset_x)
+  Shoes.app width: 360, height: 360, resizeable: false, title: 'Pirate Game' do
+    def make_items
+      items = []
+
+      items << Wave.new(self, 0, 30)
+
+      image = File.expand_path '../../imgs/pirate_ship_sm.png', __FILE__
+
+      items << Image.new(self, image, 66, 55)
+
+      [[20, 7], [40, 42], [60, -3], [80, 22]].each do |top, seed|
+        items << Wave.new(self, top, seed)
+      end
+
+      items
     end
 
-    animate(30) do |frame|
+    def draw items
       clear
+
       background Boot::COLORS[:sky]
+
       stack margin: 20 do
         title "Pirate Game!"
       end
 
-      top_of_waves = 230
-
-      draw_wave(top_of_waves, frame, 30)
-
-      draw_ship(frame, 66, 55)
-
-      draw_wave(top_of_waves+20, frame, 7)
-      draw_wave(top_of_waves+40, frame, 42)
-      draw_wave(top_of_waves+60, frame, -3)
-      draw_wave(top_of_waves+80, frame, 22)
+      items.each do |item|
+        item.draw
+      end
     end
 
+    def launch_screen
+      items = make_items
+
+      draw items
+
+      animate(30) do |frame|
+        items.each do |item|
+          item.animate frame
+        end
+      end
+    end
+
+    launch_screen
   end
 end
