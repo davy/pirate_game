c39e5574ebb53fd8af6aca7e11a1b0479d577c0b
Eric Hodel <drbrain@segment7.net>
2013-11-12 20:34:51 -0800

    Undent PirateGame::Boot
    All that wasted whitespace!

--- lib/pirate_game/boot.rb
+++ lib/pirate_game/boot.rb
@@ -1,68 +1,67 @@
 require 'shoes/color'
 require 'json'
 
-module PirateGame
-  module Boot
+module PirateGame::Boot
 
-    DARK_COLOR = '#696969' #dimgray
-    LIGHT_COLOR = '#dcdcdc' #gainsboro
-    SKY_COLOR = '#00ffff' #aqua
-    PUB_COLOR = '#52352b' #brown
+  DARK_COLOR = '#696969' #dimgray
+  LIGHT_COLOR = '#dcdcdc' #gainsboro
+  SKY_COLOR = '#00ffff' #aqua
+  PUB_COLOR = '#52352b' #brown
 
-    COLORS = {dark: DARK_COLOR, light: LIGHT_COLOR, sky: SKY_COLOR, pub: PUB_COLOR}
+  COLORS = {dark: DARK_COLOR, light: LIGHT_COLOR, sky: SKY_COLOR, pub: PUB_COLOR}
 
-    BLUE_COLORS = [
-                   Shoes::COLORS[:cornflowerblue],
-                   Shoes::COLORS[:darkcyan],
-                   Shoes::COLORS[:deepskyblue],
-                   Shoes::COLORS[:mediumturquoise],
-                   Shoes::COLORS[:steelblue],
-                   Shoes::COLORS[:teal],
-                   Shoes::COLORS[:turquoise]
-    ]
+  BLUE_COLORS = [
+    Shoes::COLORS[:cornflowerblue],
+    Shoes::COLORS[:darkcyan],
+    Shoes::COLORS[:deepskyblue],
+    Shoes::COLORS[:mediumturquoise],
+    Shoes::COLORS[:steelblue],
+    Shoes::COLORS[:teal],
+    Shoes::COLORS[:turquoise]
+  ]
 
-    GREEN_COLORS = [Shoes::COLORS[:lightseagreen],
-                    Shoes::COLORS[:mediumaquamarine],
-                    Shoes::COLORS[:mediumseagreen],
-                    Shoes::COLORS[:seagreen],
-                    Shoes::COLORS[:teal]
-    ]
+  GREEN_COLORS = [
+    Shoes::COLORS[:lightseagreen],
+    Shoes::COLORS[:mediumaquamarine],
+    Shoes::COLORS[:mediumseagreen],
+    Shoes::COLORS[:seagreen],
+    Shoes::COLORS[:teal]
+  ]
 
-    def self.config_file
-      File.expand_path '../../../config.json', __FILE__
+  def self.config_file
+    File.expand_path '../../../config.json', __FILE__
+  end
+
+  def self.config_hash
+    begin
+      JSON.parse(open(self.config_file).read)
+    rescue
+      {"stage_duration" => 30, "action_duration" => 8}
+    end
+  end
+
+  def self.config
+    @config ||= self.config_hash
+    @config
+  end
+
+  def self.waving_offset(frame, seed, delta_x, delta_y, speed = :normal)
+    t1 = frame + seed
+    t2 = frame + seed * 2
+    vel = 10.0
+
+    case speed
+    when :slow
+      vel = 20.0
+    when :fast
+      vel = 2.0
+      delta_x *= 2
+      delta_y *= 2
     end
 
-    def self.config_hash
-      begin
-        JSON.parse(open(self.config_file).read)
-      rescue
-        {"stage_duration" => 30, "action_duration" => 8}
-      end
-    end
+    offset_x = Math.sin(t1/vel) * delta_x
+    offset_y = Math.cos(t2/vel) * delta_y
 
-    def self.config
-      @config ||= self.config_hash
-      @config
-    end
-
-    def self.waving_offset(frame, seed, delta_x, delta_y, speed = :normal)
-      t1 = frame + seed
-      t2 = frame + seed * 2
-      vel = 10.0
-
-      case speed
-      when :slow
-        vel = 20.0
-      when :fast
-        vel = 2.0
-        delta_x *= 2
-        delta_y *= 2
-      end
-
-      offset_x = Math.sin(t1/vel) * delta_x
-      offset_y = Math.cos(t2/vel) * delta_y
-
-      return offset_x, offset_y
-    end
+    return offset_x, offset_y
   end
 end
