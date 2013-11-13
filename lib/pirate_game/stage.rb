14c2f7dc33b4aca46455fe13afa0c8a52d92d1ab
Davy Stevenson <davy.stevenson@gmail.com>
2013-11-12 23:01:08 -0800

    Document PirateGame::Stage

--- lib/pirate_game/stage.rb
+++ lib/pirate_game/stage.rb
@@ -1,20 +1,60 @@
 require 'pirate_command'
 
+##
+# The Stage stores information about a particular stage of
+# the game
+
 class PirateGame::Stage
 
+  ##
+  # The number of actions completed.
+
   attr_accessor :actions_completed
+
+  ##
+  # A hash storing statistics on all the players
+
   attr_accessor :player_stats
+
+  ##
+  # All possible bridge items
+
   attr_accessor :all_items
+
+  ##
+  # The time that the stage was started
+
   attr_accessor :begin_time
+
+  ##
+  # The level of the stage.
+
   attr_accessor :level
+
+  ##
+  # The number of players
+
   attr_accessor :players
 
+  ##
+  # The number of items on each player's bridge
+
   ITEMS_PER_BRIDGE = 6
+
+  ##
+  # The duration of the stage
+
   DURATION = PirateGame::Boot.config["stage_duration"]
 
   IN_PROGRESS = 'In Progress'
   SUCCESS = 'Success'
   FAILURE = 'Failure'
+
+  ##
+  # Creates a new Stage for the +level+ and number of +players+
+  #
+  # The +level+ indicates stage difficulty, and +players+ is used
+  # to determine how many bridge items to generate
 
   def initialize(level, players)
     @level = level
@@ -26,14 +66,25 @@ class PirateGame::Stage
     @begin_time = Time.now
   end
 
+  ##
+  # Creates a new Stage that is an incremented version of the
+  # current stage.
+
   def increment
     PirateGame::Stage.new self.level + 1, self.players
   end
+
+  ##
+  # The time remaining in this stage
 
   def time_left
     [0, (begin_time + DURATION) - Time.now].max
   end
 
+  ##
+  # String indicating the status of the stage.
+  #
+  # Possible values are 'In Progress', 'Success' and 'Failure'
   def status
     if time_left > 0
       IN_PROGRESS
@@ -42,17 +93,29 @@ class PirateGame::Stage
     end
   end
 
+  ##
+  # Returns true if the stage is in progress
+
   def in_progress?
     status == IN_PROGRESS
   end
+
+  ##
+  # Returns true if the stage is finished and successful
 
   def success?
     status == SUCCESS
   end
 
+  ##
+  # Returns true if the stage is finished and failed
+
   def failure?
     status == FAILURE
   end
+
+  ##
+  # Generates all the bridge items for the stage
 
   def generate_all_items
     @all_items = []
@@ -64,9 +127,18 @@ class PirateGame::Stage
     @boards = @all_items.each_slice(ITEMS_PER_BRIDGE).to_a
   end
 
+  ##
+  # Produces a unique bridge for a player in the game
+  #
+  # Should only be called once per player in the stage
+
   def bridge_for_player
     @boards.shift
   end
+
+  ##
+  # Adds an +action+ to the completed actions list, marked
+  # as completed by +from+
 
   def complete action, from
     @actions_completed += 1
@@ -74,13 +146,25 @@ class PirateGame::Stage
     @player_stats[from] << action
   end
 
+  ##
+  # The number of required actions for this stage
+  # to be a success
+
   def required_actions
     @level * 2 + 1
   end
 
+  ##
+  # Returns true if enough actions have been
+  # completed to mark the stage as passed, ie.
+  # successful
+
   def passed?
     @actions_completed >= required_actions
   end
+
+  ##
+  # Returns a hash of stage statistics
 
   def rundown
     return if status == IN_PROGRESS
