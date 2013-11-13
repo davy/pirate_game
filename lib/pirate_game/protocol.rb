1fbcef81322b534c55d9dbc23f9183a85d9e21cb
Davy Stevenson <davy.stevenson@gmail.com>
2013-11-12 22:48:18 -0800

    Document PirateGame::Protocol

--- lib/pirate_game/protocol.rb
+++ lib/pirate_game/protocol.rb
@@ -1,6 +1,13 @@
 require 'shuttlecraft/protocol'
 
+##
+# The Protocol indicates how the PirateGame signals
+# to other games via Rinda discovery
+
 class PirateGame::Protocol < Shuttlecraft::Protocol
+
+  ##
+  # The default protocol
 
   def self.default
     @@default ||= self.new :PirateGame, "Pirate Game"
