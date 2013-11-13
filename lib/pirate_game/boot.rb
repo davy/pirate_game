3ea2da80751adbae8902079a759498c08cc8d260
Eric Hodel <drbrain@segment7.net>
2013-11-12 20:41:59 -0800

    Document PirateGame::Boot

--- lib/pirate_game/boot.rb
+++ lib/pirate_game/boot.rb
@@ -1,14 +1,38 @@
 require 'shoes/color'
 require 'json'
 
+##
+# The Boot holds utility methods for starting the game.
+
 module PirateGame::Boot
 
-  DARK_COLOR = '#696969' #dimgray
-  LIGHT_COLOR = '#dcdcdc' #gainsboro
-  SKY_COLOR = '#00ffff' #aqua
-  PUB_COLOR = '#52352b' #brown
+  ##
+  # A dim gray color
+
+  DARK_COLOR = '#696969'
+
+  ##
+  # This color is called "gainsboro"
+
+  LIGHT_COLOR = '#dcdcdc'
+
+  ##
+  # The sky is aqua
+
+  SKY_COLOR = '#00ffff'
+
+  ##
+  # The pub is the brown of the grog stains on the floor
+
+  PUB_COLOR = '#52352b'
+
+  ##
+  # Colors of the game
 
   COLORS = {dark: DARK_COLOR, light: LIGHT_COLOR, sky: SKY_COLOR, pub: PUB_COLOR}
+
+  ##
+  # Various blue colors used for waves
 
   BLUE_COLORS = [
     Shoes::COLORS[:cornflowerblue],
@@ -20,6 +44,9 @@ module PirateGame::Boot
     Shoes::COLORS[:turquoise]
   ]
 
+  ##
+  # Various green colors used for waves
+
   GREEN_COLORS = [
     Shoes::COLORS[:lightseagreen],
     Shoes::COLORS[:mediumaquamarine],
@@ -28,11 +55,17 @@ module PirateGame::Boot
     Shoes::COLORS[:teal]
   ]
 
-  def self.config_file
+  ##
+  # The pirate game configuration file.
+
+  def self.config_file # :nodoc:
     File.expand_path '../../../config.json', __FILE__
   end
 
-  def self.config_hash
+  ##
+  # Loads the pirate game configuration.
+
+  def self.config_hash # :nodoc:
     begin
       JSON.parse(open(self.config_file).read)
     rescue
@@ -40,10 +73,25 @@ module PirateGame::Boot
     end
   end
 
+  ##
+  # The pirate game configuration
+
   def self.config
     @config ||= self.config_hash
     @config
   end
+
+  ##
+  # Computes the offset for waving motion of drawn items like the waves and
+  # buttons.
+  #
+  # The +frame+ is the animation frame, used to compute the offset.  The
+  # +seed+ allows randomization of the starting position of motion.  +delta_x+
+  # and +delta_y+ are the coordinates of the item at rest.  The +speed+
+  # adjusts how fast the item moves.  Valid values are <code>:normal</code> or
+  # <code>:fast</code>.
+  #
+  # Returns the x and y offsets of the item for the current frame.
 
   def self.waving_offset(frame, seed, delta_x, delta_y, speed = :normal)
     t1 = frame + seed
