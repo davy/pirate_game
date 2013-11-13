43573395691049dbe587ec0c265e60a7912e2de8
Eric Hodel <drbrain@segment7.net>
2013-11-12 22:13:49 -0800

    Document PirateGame::Bridge

--- lib/pirate_game/bridge.rb
+++ lib/pirate_game/bridge.rb
@@ -1,11 +1,33 @@
+##
+# The Bridge chooses which buttons appear on the stage screen and chooses
+# random items from all items in the game.
+
 class PirateGame::Bridge
 
-  attr_accessor :items, :stage_items
+  ##
+  # The items on the current client's bridge.
+
+  attr_accessor :items
+
+  ##
+  # All possible items in the current stage.
+
+  attr_accessor :stage_items
+
+  ##
+  # Creates a new Bridge with the clients +items+ and all +stage_items+ for
+  # the current stage.
+  #
+  # The +items+ appear as buttons in the ClientApp while the +stage_items+ are
+  # used to create actions to display.
 
   def initialize(items, stage_items)
     @items       = items
     @stage_items = stage_items
   end
+
+  ##
+  # Chooses a random item from the stage_items.
 
   def sample_item
     item = @stage_items.sample
