f4f964b8378d1a760b46c79cf1dbf76ac7d6d4b7
Eric Hodel <drbrain@segment7.net>
2013-11-19 19:01:30 -0800

    Document PirateGame::Image

--- lib/pirate_game/image.rb
+++ lib/pirate_game/image.rb
@@ -1,4 +1,15 @@
+##
+# An Image displays an image on the screen and can move like other
+# WavingItems.
+
 class PirateGame::Image < PirateGame::WavingItem
+
+  ##
+  # Creates a new WavingItem for the given +image+ that will be drawn at the
+  # +top+ and +left+ offsets.
+  #
+  # All images wave together on the screen.  There is no randomness to their
+  # motion by default.
 
   def initialize shoes, image, top, left
     super 0, 10, 4
@@ -9,11 +20,17 @@ class PirateGame::Image < PirateGame::WavingItem
     @left  = left
   end
 
+  ##
+  # Redraws the image for the given animation +frame+.
+
   def animate frame
     top_offset, left_offset = waving_offset frame
 
     @ship.move @top + top_offset, @left + left_offset
   end
+
+  ##
+  # Draws the image on the window.
 
   def draw
     @ship = @shoes.image @image, top: @top, left: @left
